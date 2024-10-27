60bea1e339383d5136e894b5adf4f70bf7695488
syukron <syukron488@gmail.com>
2024-10-27 20:23:08 +0700

    add table responsive artikel

--- resources/views/admin/berita/index.blade.php
+++ resources/views/admin/berita/index.blade.php
@@ -15,7 +15,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="card-body">
+                <div class="card-body table-responsive">
                     @if(session('status'))
                     <div class="alert alert-success">
                         {{session('status')}}
